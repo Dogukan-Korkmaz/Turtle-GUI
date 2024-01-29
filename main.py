9974e6cd87f3c1573c749d6cca6c555f5c3ce71c
DoğukanKorkmaz <dkorkmaz2000@hotmail.com>
2024-01-29 21:33:36 +0300

    Use * in importing, refactor timmy to tim, fix the installation error, add make_dashed_line() for goal, add change_color() for shape_maker() method, shape_maker is used for making triangle to decagon (all have different color)

--- main.py
+++ main.py
@@ -1,34 +1,59 @@
 # from turtle import *
 
-# import turtle
-# tim = turtle.Turtle()
+import turtle as t
+tim = t.Turtle()
+scrn = t.Screen()
 
-# import turtle as t
-# tim = t.Turtle()
-
-import heroes as h
-bum = h.gen()
-print(bum)
-
-#
-# def make_square():
-#     tim.forward(200)
-#     tim.rt(90)
-#     tim.forward(200)
-#     tim.rt(90)
-#     tim.forward(200)
-#     tim.rt(90)
-#     tim.forward(200)
-#
-#
-# tim.shape("turtle")
+tim.shape("turtle")
 # tim.color("darkblue")
-# tim.shapesize(2, 2, 5)
-# tim.speed(4)
-# make_square()
+tim.shapesize(2, 2, 5)
+tim.speed(5)
 
 
+def make_square():
+    tim.forward(200)
+    tim.rt(90)
+    tim.forward(200)
+    tim.rt(90)
+    tim.forward(200)
+    tim.rt(90)
+    tim.forward(200)
 
 
-# screen = Screen()
-# screen.exitonclick()
+def make_dashed_line():
+    for _ in range(50):
+        tim.forward(10)
+        tim.pendown()
+        tim.forward(10)
+        tim.penup()
+
+import random
+
+def change_color():
+    r = random.random()
+    b = random.random()
+    g = random.random()
+
+    tim.color(r, g, b)
+
+def shape_maker():
+    edge = 2
+    for y in range(10):
+        change_color()
+        edge += 1
+        outer_angle = 360 / edge
+        for _ in range(edge):
+            tim.right(outer_angle)
+            tim.forward(100)
+
+
+        # tim.pen(pencolor="white", pensize=1)
+        # tim.forward(10)
+        # tim.pen(pencolor="black", pensize=1)
+        # tim.forward(10)
+        # tim.pen(pencolor="white", pensize=1)
+
+# make_dashed_line()
+shape_maker()
+
+scrn.exitonclick()
